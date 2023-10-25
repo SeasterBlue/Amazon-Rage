99bd2f3f5714be5c04ef70257223ea2d1f3ebdf9
sebachoh <sebaruiz01@gmail.com>
2023-10-25 12:01:12 -0500

    added the animations, running and walking

--- Assets/Scripts/Player/Player2/PlayerController2.cs
+++ Assets/Scripts/Player/Player2/PlayerController2.cs
@@ -7,21 +7,23 @@ public class PlayerController2 : MonoBehaviour
     Rigidbody rb;
 
     #region floats
-    [SerializeField] private float currentSpeed;
-    private float rotationSpeed = 5.0f;
-    private float jumpForce = 6.0f;
+    [SerializeField] float currentSpeed;
+    float rotationSpeed = 5.0f;
+    float jumpForce = 6.0f;
     float groundDistance = 0.4f;
     #endregion
 
     #region bools
     private bool isGrounded;
-    private bool isRunning = false;
+    public bool isRunning = false;
+    private bool isMoving = false;
     #endregion
 
     #region weirdos
     private LayerMask groundMask;
     private Transform playerPickPoint;
     public Seed seed;
+    private Animator animator;
     #endregion
 
     void Start()
@@ -29,23 +31,13 @@ public class PlayerController2 : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         groundMask = LayerMask.GetMask("Ground");
         playerPickPoint = GameObject.Find("PickPoint").GetComponent<Transform>();
+        animator = GetComponent<Animator>();
         
     }
 
     private void Update()
     {
         CheckIfGrounded();
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        currentSpeed = Input.GetKey(KeyCode.LeftShift) ? 10.0f : 5.0f;
-        isRunning = currentSpeed == 10.0f;
-        
-        HandleMovement(currentSpeed);
-
-
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             HandleJump();
@@ -56,16 +48,33 @@ public class PlayerController2 : MonoBehaviour
         }
     }
 
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        currentSpeed = Input.GetKey(KeyCode.LeftShift) ? 10.0f : 5.0f;
+        isRunning = currentSpeed == 10.0f;
+
+        HandleMovement(currentSpeed);
+
+        
+       
+    }
+
     void HandleMovement(float moveSpeed)
     {
 
         Vector2 inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        bool isMoving = inputVector != Vector2.zero;
+
+        animator.SetBool("isWalking", isMoving && !isRunning);
+        animator.SetBool("isRunning", isRunning);
+
+        //
         Vector3 moveDirection = new(inputVector.x, 0.0f, inputVector.y);
         float moveDistance = moveSpeed * Time.deltaTime;
 
-         transform.position += moveDirection * moveDistance;
-       
-
+        //Movement
+        transform.position += moveDirection * moveDistance;
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
     }
 
