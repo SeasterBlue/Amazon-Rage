e7d377779a196a33a318401da7719dd26e35afe8
Batman-exe <s.tangarife@uniandes.edu.co>
2023-10-27 13:18:27 -0500

    idle walk animations

--- Assets/Scripts/Lumberjack.cs
+++ Assets/Scripts/Lumberjack.cs
@@ -8,6 +8,7 @@ public class Lumberjack : RecyclableObject
 {
     private int health;
     private NavMeshAgent navAgent;
+    private Animator animator;
 
     internal override void Init()
     {
@@ -15,6 +16,8 @@ public class Lumberjack : RecyclableObject
         Invoke(nameof(Recycle), 15);
         if(navAgent == null)
             navAgent = GetComponent<NavMeshAgent>();
+        if(animator == null)
+            animator = GetComponent<Animator>();
     }
 
     internal override void Release()
@@ -27,13 +30,32 @@ public class Lumberjack : RecyclableObject
         if(other.gameObject.CompareTag("Player"))
         {
             navAgent.isStopped = false;
-            navAgent.destination = other.gameObject.transform.position;
+            //navAgent.updateRotation = true;
+            animator.SetBool("walking", true);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Vector3 destination = other.gameObject.transform.position;
+            navAgent.destination = destination;
+            
+            Vector3 direction = (destination - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * navAgent.angularSpeed);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
+        {
+            navAgent.destination = transform.position;
             navAgent.isStopped = true;
+            //navAgent.updateRotation = false;
+            animator.SetBool("walking", false);
+        }
     }
 }
