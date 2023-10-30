f5d096b7c2a2033ca9c9af1a2c994093266ec11d
sebachoh <sebaruiz01@gmail.com>
2023-10-30 17:05:34 -0500

    trying to control brightness

--- Assets/Scripts/Management/GameManager.cs
+++ Assets/Scripts/Management/GameManager.cs
@@ -1,15 +1,20 @@
 using System;
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
 
+    GameObject leaf;
     Transform finalSpot;
     Transform magicPlant;
-
     public float distanceMagnitude;
+    public float minDistance = 2;
+    public float maxDistance = 160;
+    Renderer plantRenderer;
+    Material plantMaterial;
 
 
 
@@ -38,16 +43,30 @@ public class GameManager : MonoBehaviour
     {
         finalSpot = GameObject.Find("FinalSpot").GetComponent<Transform>();
         magicPlant = GameObject.Find("MagicPlant").GetComponent<Transform>();
+        leaf = GameObject.Find("Box037").GetComponent<GameObject>();
+        plantRenderer = leaf.GetComponent<Renderer>();
+        plantMaterial = plantRenderer.GetComponent<Material>();
     }
 
     void Update()
     {
         CalcuteDistance();
+        ControlBrightnessBasedOnDistance();
     }
 
     public void CalcuteDistance()
     {
         distanceMagnitude = finalSpot.position.magnitude - magicPlant.position.magnitude;
+        
+    }
+
+    public void ControlBrightnessBasedOnDistance()
+    {
+        float brightnessFactor = Mathf.InverseLerp(minDistance, maxDistance, distanceMagnitude);
+        Color originalColor = plantMaterial.color;
+        Color finalColor = Color.Lerp(originalColor, Color.yellow, brightnessFactor);
+        plantMaterial.color = finalColor;
+
     }
 
 
