e8a758c2eaa6e70fa51573ef0c3168d76723b33f
Samuel Eduardo Álvarez Sepúlveda <71793961+SeasterBlue@users.noreply.github.com>
2023-11-03 22:26:42 -0500

    UI input system and pause button fixed
    UI pause button removed. Player can only access puse menu by pressing Esc.

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -8,34 +8,60 @@ public class Pause : MonoBehaviour
 {
     [SerializeField] private Button pauseBtn;
     [SerializeField] private Button resumeBtn;
+    [SerializeField] private GameObject canvas;
+    [SerializeField] private GameObject audioObject;
+
+    private AudioSource sfx;
+
     private bool pause = false;
+
+    private void Start()
+    {
+        sfx = audioObject.GetComponent<AudioSource>(); 
+        canvas.SetActive(false);
+    }
 
     private void LateUpdate()
     {
+        PauseToggle();
+    }
 
+    void PauseToggle()
+    {
         if (Input.GetKeyUp(KeyCode.Escape))
         {
-            if (!pause)
+            if (pause)
             {
-                pause = true;
-                pauseBtn.onClick.Invoke();
-            } else
+                ResumeGame();
+            }
+            else
             {
-                resumeBtn.onClick.Invoke();
-                pause = false;
+                PauseGame();
             }
         }
     }
 
     public void PauseGame()
     {
+        //pauseBtn.enabled = false;
+        canvas.SetActive(true);
+
         // Time.timeScale = 0f;
         Debug.Log("Time Pause");
+
+        sfx.Play();
+        pause = true;
     }
 
     public void ResumeGame()
     {
+        //pauseBtn.enabled = true;
+        canvas.SetActive(false);
+
         // Time.timeScale = 1f;
         Debug.Log("Time Running");
+
+        sfx.Play();
+        pause = false;
     }
 }
